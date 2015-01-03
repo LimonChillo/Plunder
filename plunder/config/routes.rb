969838e7748ea707472c701b5c8b674bd6ef420c
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-01-03 16:59:33 +0100

    added Devise User Auth"
    "

--- plunder/config/routes.rb
+++ plunder/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :users
   get 'product/show'
   get 'product/edit'
 
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to =>'product#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
