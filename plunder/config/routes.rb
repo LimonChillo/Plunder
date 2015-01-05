250aefad47e2d68805c099072005a6f0300008d7
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-01-05 23:20:29 +0100

    part of location edit

--- plunder/config/routes.rb
+++ plunder/config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   #match "/matches/:id" => "articles#matches"
   #get 'articles/random' => 'articles#random'
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-
+  #devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- plunder/db/schema.rb
+++ plunder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105190930) do
+ActiveRecord::Schema.define(version: 20150105205711) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150105190930) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
