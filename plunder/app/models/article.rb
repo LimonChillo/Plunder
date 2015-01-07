2f66cdbe316a609922909509af8d821594cfe294
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-01-07 16:39:35 +0100

    cropping resulted in auto-crop

--- plunder/app/models/article.rb
+++ plunder/app/models/article.rb
@@ -3,8 +3,22 @@ class Article < ActiveRecord::Base
   has_many :users, :through => :matches
   belongs_to :user
 
-  has_attached_file :avatar, :styles => { :large => "600x600", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :large => "600x600", :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   #crop_attached_file :avatar
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  #after_crop :reprocess_avatar, :if => :cropping?
+
+  # def cropping?(id = 0)
+  #   !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  # end
+
+
+  # def self.reprocess_avatar
+  #   #Article.where(:id => params[:id]).first.update_avatar(:crop_x => nil)
+
+
+
+  #   crop_x = nil
+  #   redirect_to random_article_path
+  # end
 end
