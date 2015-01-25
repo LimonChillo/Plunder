7f43383e8f37ae8b97d216fdb1f91575b6bff8fe
David Kranewitter <dkranewitter.mmt-b2013@fh-salzburg.ac.at>
2015-01-25 22:51:33 +0100

    added mail confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,17 +45,16 @@ Rails.application.configure do
   config.app_domain = 'plunder.com'
 
   # Email
+  config.action_mailer.default_url_options = {:host => 'www.plunder.club'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'someuser',
-    password: 'somepass',
-    authentication: :plain,
-    domain: 'plunder.com'
+    :user_name => 'RaketenWirt',
+    :password => ENV["sendgrid_key"],
+    :domain => 'www.plunder.club',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   #For Devise
